a8d4c158610e0accfed743430fdee03bbc098f38
Luiz Ferraz <luiz@lferraz.com>
2023-07-01 03:34:11 -0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,52 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::borrow::Cow;
+
+use actix_web::{get, post, web::Json, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use diesel::{table, Insertable};
+use serde::{Deserialize, Serialize};
+use log::debug;
+
+table! {
+    applied_item (id) {
+        id -> Text,
+        name -> Text,
+        amount -> Integer,
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Insertable)]
+#[diesel(table_name = applied_item)]
+struct AppliedItem<'a> {
+    id: Cow<'a, str>,
+    name: Cow<'a, str>,
+    amount: i32,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[post("/set_items")]
+async fn set_items(Json(body): Json<Vec<AppliedItem<'_>>>) -> impl Responder {
+    println!("{body:?}");
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body("OK")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "francis=debug,actix_web=info");
+    pretty_env_logger::init();
+
+    debug!("Starting server");
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(set_items)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8088))?
     .run()
     .await
 }
